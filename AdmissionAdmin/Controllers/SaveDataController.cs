c81d5b692332af8f2b705110c5c8320316bdd558
91784 <91784@DEEPAK>
2020-07-23 13:12:48 +0530

    Add project files.

---
+++ AdmissionAdmin/Controllers/SaveDataController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdmissionAdmin.Models;
+
+namespace AdmissionAdmin.Controllers
+{
+    public class SaveDataController : Controller
+    {
+        // GET: SaveData
+        public ActionResult Index()
+        {
+            ViewBag.saveresult = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(StudentData student)
+        {
+            using (collegedataEntities entities = new collegedataEntities())
+            {
+                entities.StudentDatas.Add(student);
+                entities.SaveChanges();
+                ViewBag.saveresult = "Data Has Been Saved Successfully";
+                ModelState.Clear();
+            }
+
+            return View(new StudentData());
+        }
+    }
+}
---
+++ AdmissionAdmin/Controllers/StudentDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdmissionAdmin.Controllers
+{
+    public class StudentDetailsController : Controller
+    {
+        // GET: StudentDetails
+        public ActionResult StusentDetails()
+        {
+            return View();
+        }
+    }
+}
